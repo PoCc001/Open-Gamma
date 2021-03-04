bd2b4223ba632bc5ad12e21b7f6b54bd0090cc4b
PoCc001 <49920248+PoCc001@users.noreply.github.com>
2021-03-04 20:47:09 +0100

    add an optional better approximation of loggamma
    this makes the functions potentially slower, but more accurate

--- fastgamma.h
+++ fastgamma.h
@@ -37,6 +37,17 @@
 #define FUNC_INLINE
 #endif
 
+#define USE_APPROX_ARRAY 1 // change to 0 to make fast_loggamma even faster but more incorrect
+
+// array to correct the approximation of the logarithm in fast_loggamma
+double LD_ARRAY [] = {0.0, 0.04439411, 0.08746284, 0.129283,
+	0.169925, 0.2094534, 0.2479275, 0.2854022, 0.3219281, 
+	0.357552, 0.3923174, 0.4262647, 0.4594316, 0.4918531,
+	0.523562, 0.5545889, 0.5849625, 0.6147098, 0.6438562,
+	0.672425, 0.7004397, 0.7279205, 0.7548875, 0.7813597,
+	0.8073549, 0.83289, 0.857981, 0.882643, 0.9068906,
+	0.9307373, 0.9541963, 0.9772799};
+
 /*
  * Two unions to enable bit manipulation
  * on floating-point arguments, so that a
@@ -87,11 +98,19 @@ FUNC_INLINE float fast_gammaf(float x) {
 FUNC_INLINE double fast_loggamma(double x) {
 	union double_to_i64 ux;
 	ux.d = x;
+#if USE_APPROX_ARRAY == 0
 	ux.i &= (uint64_t)(0x3fffffffffffffffULL);
+#else
+	ux.i -= (uint64_t)(0x3ff0000000000000ULL);
+	unsigned int array_index = (unsigned int)(ux.i >> 47) & 0b11111;
+#endif
 	ux.i >>= 52;
 	double log = (double)(ux.i);
-	log *= LOG_2_D; // a rough estimate of the natural log of x - 1
-	return x * log;
+#if USE_APPROX_ARRAY != 0
+	log += LD_ARRAY[array_index];
+#endif
+	log *= LOG_2_D; // a rough estimate of the natural log of x
+	return x * (log - 1.0);
 }
 
 /*
@@ -104,10 +123,18 @@ FUNC_INLINE double fast_loggamma(double x) {
 FUNC_INLINE float fast_loggammaf(float x) {
 	union float_to_i32 ux;
 	ux.f = x;
+#if USE_APPROX_ARRAY == 0
 	ux.i &= (uint32_t)(0x3fffffffUL);
+#else
+	ux.i -= (uint32_t)(0x3f800000UL);
+	unsigned int array_index = (unsigned int)(ux.i >> 18) & 0b11111;
+#endif
 	ux.i >>= 23;
 	float log = (float)(ux.i);
-	log *= LOG_2_F; // a rough estimate of the natural log of x - 1
+#if USE_APPROX_ARRAY != 0
+	log += (float)(LD_ARRAY[array_index]);
+#endif
+	log *= LOG_2_F; // a rough estimate of the natural log of x
 	return x * log;
 }
 
